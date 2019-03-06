c81043d200a421ec30c0ae9916a20e341f3048c2
Hocine Hammar <h.hocinee@hotmail.fr>
2019-03-06 17:48:37 +0100

    j'ai fait un petit projet JPA en utilisant Hibernate, il ne marche pas et je n'arrive pas a trouver la solution !!!! :(

---
+++ JPA.hibernate/src/main/java/com/JPA/HIBERNATE/App/App.java
@@ -0,0 +1,53 @@
+package com.JPA.HIBERNATE.App;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.JPA.HIBERNATE.Library.Author;
+import com.JPA.HIBERNATE.Library.Book;
+
+public class App {
+	private static Logger log = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		System.out.println("Runing App...");
+		log.debug("Create persistence manager");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("maApp");
+		EntityManager em = emf.createEntityManager();
+		log.debug("Create entities");
+		Author Erik = new Author();
+		Erik.setFirstName("Erik");
+		Erik.setLastName("Lionel");
+		
+		Author Yahia = new Author();
+		Yahia.setFirstName("Yahia");
+		Yahia.setLastName("Touré");
+		
+		Book book = new Book();
+		book.setIsbn("897-45-456678-43");
+		book.setTitle("JAVA JEE JPA HIBERNATE");
+		book.setPrice(70.80);
+		
+		book.getAuthors().add(Erik);
+		book.getAuthors().add(Yahia);
+		log.debug("Persistence entities in a transaction");
+		
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.persist(Erik);
+		em.persist(Yahia);
+		em.persist(book);
+		
+		transaction.commit();
+		log.debug("Close Entity Manager");
+		em.close();
+		emf.close();
+
+	}
+
+}
---
+++ JPA.hibernate/src/main/java/com/JPA/HIBERNATE/Library/Book.java
@@ -0,0 +1,51 @@
+package com.JPA.HIBERNATE.Library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+	
+	@Id
+	
+	private String isbn;
+	private String title;
+	private Double price;
+	
+	
+	public List<Author> getAuthors() {
+		return authors;
+	}
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+	@ManyToMany
+	List<Author> authors = new ArrayList<>();
+	
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+
+}

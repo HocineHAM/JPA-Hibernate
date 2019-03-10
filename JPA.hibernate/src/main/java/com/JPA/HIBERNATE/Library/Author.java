65cc4c171be748c9725124a27e9c9261fe9eea4a
yahya <m2i.abdellahyaya@gmail.com>
2019-03-10 15:27:39 +0100

    Correction du bug : yahya

--- JPA.hibernate/src/main/java/com/JPA/HIBERNATE/Library/Author.java
+++ JPA.hibernate/src/main/java/com/JPA/HIBERNATE/Library/Author.java
@@ -2,13 +2,14 @@ package com.JPA.HIBERNATE.Library;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Author {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String firstName;
 	private String lastName;
